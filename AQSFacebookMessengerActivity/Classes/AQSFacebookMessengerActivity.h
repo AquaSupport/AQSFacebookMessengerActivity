ac736976ca705fafa439cacec428d82d233a8f21
Kai INUI <lied.der.optik@gmail.com>
2014-11-11 15:33:14 +0900

    Some documentations

--- AQSFacebookMessengerActivity/Classes/AQSFacebookMessengerActivity.h
+++ AQSFacebookMessengerActivity/Classes/AQSFacebookMessengerActivity.h
@@ -8,6 +8,14 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  @warning `AQSFacebookMessengerActivity` requires setup!
+ *
+ *  1. Register Facebook App https://developers.facebook.com/apps/
+ *  3. In your .plist, set FacebookAppID as your FB App ID. (like 7828392093)
+ *  4. In your .plist, set FacebookDisplayName as you FB App's Display Name.
+ *  5. Setup URL Scheme like fb42 where 42 is your FB App ID (For FB App ID:7828392093, the URL Scheme is fb7828392093)
+ */
 @interface AQSFacebookMessengerActivity : UIActivity
 
 @end
